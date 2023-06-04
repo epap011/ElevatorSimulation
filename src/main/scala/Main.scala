38cd854b73667af03126fec9fedfe04e94877e0a
Efthymis Papageorgiou <papageorgiou.efthymis@protonmail.com>
2023-06-04 21:51:49 +0300

    feat: validate input arguments
    Elevators, Roofs and People must be positive integers

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,26 @@
+import scala.util.{Try,Success,Failure}
+
 object Main extends App {
-  println("Elevator Simulation")
+
+    val elevatorsNumTry:Try[Int] = toInt(args(0))
+    val floorsNumTry   :Try[Int] = toInt(args(1))
+    val peopleNumTry   :Try[Int] = toInt(args(2))
+
+    if(elevatorsNumTry.isFailure 
+    || floorsNumTry.isFailure
+    || peopleNumTry.isFailure) {
+        println("[Error] Please provide 3 integer arguments")
+        System.exit(1)
+    }
+
+    val elevatorsNum = elevatorsNumTry.get
+    val floorsNum    = floorsNumTry.get
+    val peopleNum    = peopleNumTry.get
+
+    if(elevatorsNum < 1 || floorsNum < 1 || peopleNum < 1) {
+        println("[Error] Please provide 3 positive integer arguments")
+        System.exit(1)
+    }
+
+    def toInt(s: String): Try[Int] = Try(Integer.parseInt(s.trim))
 }
