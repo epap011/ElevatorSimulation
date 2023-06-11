e05d85ef78d2178bd3d0b00c3721e03725e0bec9
Efthymis Papageorgiou <papageorgiou.efthymis@protonmail.com>
2023-06-11 10:47:33 +0300

    feat: Building Actor

--- src/main/scala/Building.scala
+++ src/main/scala/Building.scala
@@ -1,7 +1,39 @@
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.AbstractBehavior
+import akka.actor.typed.scaladsl.ActorContext
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.scaladsl.LoggerOps
-import akka.actor.typed.{ ActorRef, ActorSystem, Behavior }
+import akka.actor.typed.Signal
+import akka.actor.typed.PostStop
 
 object Building {
+    def apply(): Behavior[BuildingMessage] =
+        Behaviors.setup(context => new Building(context))
     
+    sealed trait BuildingMessage
+    final case class StartBuilding(elevatorsNum: Int, floorsNum: Int, passengersNum: Int) extends BuildingMessage
+}
+
+class Building(context: ActorContext[Building.BuildingMessage]) extends AbstractBehavior[Building.BuildingMessage](context) {
+    override def onMessage(msg: Building.BuildingMessage): Behavior[Building.BuildingMessage] = {
+        msg match {
+            case Building.StartBuilding(elevatorsNum, floorsNum, passengersNum) => 
+                val elevatorActors = (1 to numberOfElevators).map { elevatorId =>
+                    context.spawn(Elevator(elevatorId), s"elevator-$elevatorId")
+                }
+                val floorActors = (1 to floorsNum).map { floorId =>
+                    context.spawn(Floor(floorId), s"floor-$floorId")
+                }
+                val passengerActors = (1 to passengersNum).map { passengerId =>
+                    context.spawn(Passenger(passengerId), s"passenger-$passengerId")
+                }
+                this
+        }
+    }
+
+    override def onSignal: PartialFunction[Signal, Behavior[Building.BuildingMessage]] = {
+        case PostStop =>
+            context.log.info("Building stopped")
+            this
+    }
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,29 +1,30 @@
 import scala.util.{Try,Success,Failure}
-import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.typed.scaladsl.LoggerOps
-import akka.actor.typed.{ ActorRef, ActorSystem, Behavior }
+import akka.actor.typed.ActorSystem
 
 object Main extends App {
 
-    val elevatorsNumTry:Try[Int] = toInt(args(0))
-    val floorsNumTry   :Try[Int] = toInt(args(1))
-    val peopleNumTry   :Try[Int] = toInt(args(2))
+    val elevatorsNumTry  :Try[Int] = toInt(args(0))
+    val floorsNumTry     :Try[Int] = toInt(args(1))
+    val passengersNumTry :Try[Int] = toInt(args(2))
 
     if(elevatorsNumTry.isFailure 
     || floorsNumTry.isFailure
-    || peopleNumTry.isFailure) {
+    || passengersNumTry.isFailure) {
         println("[Error] Please provide 3 integer arguments")
         System.exit(1)
     }
 
-    val elevatorsNum = elevatorsNumTry.get
-    val floorsNum    = floorsNumTry.get
-    val peopleNum    = peopleNumTry.get
+    val elevatorsNum  = elevatorsNumTry.get
+    val floorsNum     = floorsNumTry.get
+    val passengersNum = passengersNumTry.get
 
-    if(elevatorsNum < 1 || floorsNum < 1 || peopleNum < 1) {
+    if(elevatorsNum < 1 || floorsNum < 1 || passengersNum < 1) {
         println("[Error] Please provide 3 positive integer arguments")
         System.exit(1)
     }
+
+    val system: ActorSystem[Building.StartBuilding] = ActorSystem(Building(), "building")
+    system ! Building.StartBuilding(elevatorsNum, floorsNum, passengersNum)
 
     def toInt(s: String): Try[Int] = Try(Integer.parseInt(s.trim))
 }
