d8537b478cd36e5818cdb58a049fa724f7e95f05
Efthymis Papageorgiou <papageorgiou.efthymis@protonmail.com>
2023-06-15 10:36:43 +0300

    refactor(elevator): elevator consists of three states (idle, waiting, travelling)

--- src/main/scala/Elevator.scala
+++ src/main/scala/Elevator.scala
@@ -5,6 +5,7 @@ import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.scaladsl.LoggerOps
 import akka.actor.typed.scaladsl.TimerScheduler
 import scala.concurrent.duration._
+import scala.collection.immutable.SortedSet
 
 object Elevator {
     sealed trait Command
@@ -26,42 +27,62 @@ class Elevator(
     buffer: StashBuffer[Elevator.Command],
     elevatorID: Int) {
     
-    val timePerFloor: Int = 1000*5
+    val timePerFloor: Int = 1
     var currentFloor: Int = 0
+
+    var floorCallRequests = SortedSet[Int]()
 
     private def start: Behavior[Elevator.Command] = idle
 
     private def idle: Behavior[Elevator.Command] = {
+        context.log.info(s"[Elevator $elevatorID]: Idle")
+        Behaviors.receiveMessage {
+            case Elevator.CallElevator(floor) =>
+                context.log.info(s"[Elevator $elevatorID]: received CallElevator to floor $floor")
+                floorCallRequests += floor
+                waiting
+            
+            case Elevator.Timeout => Behaviors.same
+        }
+    }
+
+    private def waiting: Behavior[Elevator.Command] = {
+        context.log.info(s"[Elevator $elevatorID]: Waiting")
         Behaviors.withTimers[Elevator.Command] { timers =>
             timers.startSingleTimer(Elevator.Timeout, 10.seconds)
-            context.log.info(s"[Elevator $elevatorID]: Doors opened!")
             Behaviors.receiveMessage {
                 case Elevator.CallElevator(floor) =>
                     context.log.info(s"[Elevator $elevatorID]: received CallElevator to floor $floor")
+                    floorCallRequests += floor
                     Behaviors.same
                 
                 case Elevator.Timeout =>
-                    context.log.info(s"[Elevator $elevatorID]: Doors closed!")
                     timers.cancel(Elevator.Timeout)
-                    Behaviors.same
+                    if(floorCallRequests.isEmpty) idle
+                    else travelling
             }
         }
     }
 
-    private def travelToFloor(floorNumber: Int): Unit = {
-        if (currentFloor < floorNumber) {
-            while (currentFloor < floorNumber) {
-                Thread.sleep(timePerFloor)
-                currentFloor += 1
-            }
-        } 
-        else 
-        if (currentFloor > floorNumber) {
-            while (currentFloor > floorNumber) {
-                currentFloor -= 1
-                Thread.sleep(timePerFloor)
+    private def travelling: Behavior[Elevator.Command] = {
+        context.log.info(s"[Elevator $elevatorID]: Travelling")
+        Behaviors.withTimers[Elevator.Command] { timers =>
+            val nextFloor = floorCallRequests.head
+            floorCallRequests -= nextFloor
+            val timeToNextFloor = (nextFloor - currentFloor).abs * timePerFloor
+            timers.startSingleTimer(Elevator.Timeout, timeToNextFloor.seconds)
+            Behaviors.receiveMessage {
+                case Elevator.CallElevator(floor) =>
+                    context.log.info(s"[Elevator $elevatorID]: received CallElevator to floor $floor")
+                    floorCallRequests += floor
+                    Behaviors.same
+                
+                case Elevator.Timeout =>
+                    timers.cancel(Elevator.Timeout)
+                    context.log.info(s"[Elevator $elevatorID]: reaches floor $nextFloor!")
+                    currentFloor = nextFloor
+                    waiting
             }
         }
-
     }
 }
